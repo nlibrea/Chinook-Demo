6bda2c9288b7c8af10361e21f1f4576a5dcc98e7
nlibrea <nlibrea@ymail.com>
2016-10-04 16:08:26 -0600

    Create CustomerManager and Customer Profile class

---
+++ Chinook Demo/Chinook.Framework/BLL/CustomerManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chinook.Framework.DAL; // for the ChinookCOntext class
+using Chinook.Framework.Entities; // for the EF entities
+using Chinook.Framework.Entities.ViewModels; // for my DTOs/ POCOs, etc.
+
+namespace Chinook.Framework.BLL
+{
+    public class CustomerManager
+    {
+        public CustomerProfile GetProfile(int customerId)
+        {
+            // Access the database
+            using (var context = new ChinookContext())
+            {
+                var profile = from person in context.Customers
+                              where person.CustomerId == customerId
+                              select new CustomerProfile()
+                              {
+                                  FirstName = person.FirstName,
+                                  LastName = person.LastName,
+                                  CompanyName = person.Company,
+                                  StreetAddress = person.Address,
+                                  City = person.City,
+                                  State = person.State,
+                                  Country = person.Country,
+                                  PostalCode = person.PostalCode
+                              };
+
+                return profile.Single();
+            }
+
+
+        }
+    }
+}
