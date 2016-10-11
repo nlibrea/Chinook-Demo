643540fe0b9d90f321b67ffcd400adccec0414d5
nlibrea <nlibrea@ymail.com>
2016-10-11 16:56:01 -0600

    add AddWebMaster() in UserManager

--- Chinook Demo/Chinook.Framework/BLL/Security/UserManager.cs
+++ Chinook Demo/Chinook.Framework/BLL/Security/UserManager.cs
@@ -16,5 +16,29 @@ namespace Chinook.Framework.BLL.Security
             : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
         {
         }
+
+        #region Constants
+        private const string STR_DEFAULT_PASSWORD = "Pa$$word1";
+        /// <summary>Requires FirstName and LastName</summary>
+        private const string STR_USERNAME_FORMAT = "{0}.{1}";
+        /// <summary>Requires UserName</summary>
+        private const string STR_EMAIL_FORMAT = "{0}@ChinookLab.tba";
+        private const string STR_WEBMASTER_USERNAME = "Webmaster";
+        #endregion
+
+        public void AddWebMaster()
+        {
+            if(!Users.Any(u=>u.UserName.Equals(STR_WEBMASTER_USERNAME)))
+            {
+                var webMasterAccount = new ApplicationUser()
+                {
+                    UserName = STR_WEBMASTER_USERNAME,
+                    Email = string.Format(STR_EMAIL_FORMAT, STR_WEBMASTER_USERNAME)
+                };
+                this.Create(webMasterAccount, STR_DEFAULT_PASSWORD);
+                this.AddToRole(webMasterAccount.Id, SecurityRoles.WebsiteAdmins);
+
+            }
+        } // end AddWebMaster()
     }
 }
