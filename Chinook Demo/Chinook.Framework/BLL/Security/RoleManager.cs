fbbaaf4420660df12f199164c7bd86e3330733f0
nlibrea <nlibrea@ymail.com>
2016-10-11 16:45:44 -0600

    Add SecurityRoles class + update RoleManager.cs

--- Chinook Demo/Chinook.Framework/BLL/Security/RoleManager.cs
+++ Chinook Demo/Chinook.Framework/BLL/Security/RoleManager.cs
@@ -1,6 +1,8 @@
 ﻿using Chinook.Framework.DAL.Security;
+using Chinook.Framework.Entities.Security;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Linq;
 
 namespace Chinook.Framework.BLL.Security
 {
@@ -9,7 +11,18 @@ namespace Chinook.Framework.BLL.Security
         public RoleManager()
             : base(new RoleStore<IdentityRole>(new ApplicationDbContext()))
         {
-
         }
+
+        public void AddDefaultRoles()
+        {
+            foreach (string roleName in SecurityRoles.DefaultSecurityRoles)
+            {
+                // Check if it exists
+                if (!Roles.Any(r => r.Name == roleName))
+                    this.Create(new IdentityRole(roleName));
+            }
+        }
+
+        
     }
 }
