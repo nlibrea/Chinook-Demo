a4df8950753bb6c09bbc2fe8b9a5645da3fe7da6
nlibrea <nlibrea@ymail.com>
2016-10-25 15:30:13 -0600

    manage Unregistered Users - class + method

--- Chinook Demo/Chinook.Framework/BLL/Security/UserManager.cs
+++ Chinook Demo/Chinook.Framework/BLL/Security/UserManager.cs
@@ -122,5 +122,87 @@ namespace Chinook.Framework.BLL.Security
 
         #endregion
 
+        #region Business Processing Operations
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<UnregisteredUser> ListAllUnregisteredUsers()
+        {
+            using (var context = new ChinookContext())
+            {
+                // Make an in-memory list of employees who have login accounts
+                var registeredEmployees = (from emp in Users
+                                           where emp.EmployeeID.HasValue
+                                           select emp.EmployeeID).ToList();
+                // Query employees who don't have login accounts.
+                // Make it in-memory (.ToList()) for the next step of assigning usernames/emails
+                var employees = (from emp in context.Employees
+                                 where !registeredEmployees.Any(e => emp.EmployeeId == e)
+                                 select new UnregisteredUser()
+                                 {
+                                     Id = emp.EmployeeId,
+                                     FirstName = emp.FirstName,
+                                     LastName = emp.LastName,
+                                     UserType = UnregisteredUserType.Employee
+                                 }).ToList();
+                // Assign employee usernames and emails
+                foreach (var person in employees)
+                {
+                    person.AssignedUserName = string.Format(STR_USERNAME_FORMAT, person.FirstName, person.LastName);
+                    person.AssignedEmail = string.Format(STR_EMAIL_FORMAT, person.AssignedUserName);
+                }
+
+                // Make an in-memory list of customers who have login accounts
+                var registeredCustomers = (from cust in Users
+                                           where cust.CustomerID != null
+                                           select cust.CustomerID).ToList();
+                // Query customers who don't have login accounts.
+                var customers = from cust in context.Customers
+                                where !registeredCustomers.Any(c => cust.CustomerId == c)
+                                select new UnregisteredUser()
+                                {
+                                    Id = cust.CustomerId,
+                                    FirstName = cust.FirstName,
+                                    LastName = cust.LastName,
+                                    UserType = UnregisteredUserType.Customer
+                                };
+
+                // Merge and return the results
+                return employees.Union(customers).ToList();
+            }
+        }
+
+
+        public void RegisterUser(UnregisteredUser userInfo)
+        {
+            // string randomPassword = Guid.NewGuid().ToString().Replace("-", "");
+            var userAccount = new ApplicationUser()
+            {
+                UserName = userInfo.AssignedUserName,
+                Email = userInfo.AssignedEmail
+            };
+            switch (userInfo.UserType)
+            {
+                case UnregisteredUserType.Customer:
+                    userAccount.CustomerID = userInfo.Id;
+                    break;
+                case UnregisteredUserType.Employee:
+                    userAccount.EmployeeID = userInfo.Id;
+                    break;
+            }
+
+            this.Create(userAccount, STR_DEFAULT_PASSWORD); // or randomPassword
+            switch (userInfo.UserType)
+            {
+                case UnregisteredUserType.Employee:
+                    this.AddToRole(userAccount.Id, SecurityRoles.Staff);
+                    break;
+                case UnregisteredUserType.Customer:
+                    this.AddToRole(userAccount.Id, SecurityRoles.RegisteredUsers);
+                    break;
+            }
+        }
+
+
+
+        #endregion
     }
 }
---
+++ Chinook Demo/Chinook.Framework/Entities/Security/UnregisteredUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinook.Framework.Entities.Security
+{
+    public enum UnregisteredUserType { Undefined, Employee, Customer }
+    public class UnregisteredUser
+    {
+        public int Id { get; set; }
+        public UnregisteredUserType UserType { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AssignedUserName { get; set; }
+        public string AssignedEmail { get; set; }
+    }
+}
