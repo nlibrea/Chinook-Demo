eb83641a04642de2c348ff57b6ee616250f3c6bf
nlibrea <nlibrea@ymail.com>
2016-11-03 16:00:02 -0600

    Validation attributes example on track entity

--- Chinook Demo/Chinook.Framework/Entities/Track.cs
+++ Chinook Demo/Chinook.Framework/Entities/Track.cs
@@ -6,22 +6,26 @@
 
 namespace Chinook.Framework.Entities
 {
-    [Table("Track")]
+    [Table("Track")] // Identifies which DB table this class maps to
     public partial class Track
     {
+        [Key] // Identifies that this property maps as a ProimaryKey in the DB
         public int TrackId { get; set; }
 
-        [Required]
-        [StringLength(200)]
+        [Required(ErrorMessage ="Name is a required field.")] //  This Property cant have a null value
+        [StringLength(200, ErrorMessage = "Name is too long. Max length is 200 characters.")]
         public string Name { get; set; }
 
+        [Range(1, int.MaxValue, ErrorMessage = "Album is invalid.")] // This property's value must fit in the range of min/max values
         public int? AlbumId { get; set; }
 
+        [Range(1, int.MaxValue)]
         public int MediaTypeId { get; set; }
 
+        [Range(1, int.MaxValue)]
         public int? GenreId { get; set; }
 
-        [StringLength(220)]
+        [StringLength(220, MinimumLength = 3)]
         public string Composer { get; set; }
 
         public int Milliseconds { get; set; }
